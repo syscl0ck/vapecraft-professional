c7db79d3c470ad0ba40bc72e8c09a26ce5892dec
syscl0ck <instantraptors@protonmail.com>
2025-07-29 22:40:57 +0000

    Uncommented Plugin Download Script

--- plugins/download.sh
+++ plugins/download.sh
@@ -5,41 +5,41 @@
 set -e
 
 # Multiverse-Core (example version 4.3.2)
-# curl -o Multiverse-Core.jar -L https://github.com/Multiverse/Multiverse-Core/releases/download/v4.3.2/Multiverse-Core-4.3.2.jar
+curl -o Multiverse-Core.jar -L https://github.com/Multiverse/Multiverse-Core/releases/download/v4.3.2/Multiverse-Core-4.3.2.jar
 
 # Multiverse-Portals
-# curl -o Multiverse-Portals.jar -L https://github.com/Multiverse/Multiverse-Portals/releases/download/v4.2.1/Multiverse-Portals-4.2.1.jar
+curl -o Multiverse-Portals.jar -L https://github.com/Multiverse/Multiverse-Portals/releases/download/v4.2.1/Multiverse-Portals-4.2.1.jar
 
 # Vault
-# curl -o Vault.jar -L https://github.com/MilkBowl/Vault/releases/latest/download/Vault.jar
+curl -o Vault.jar -L https://github.com/MilkBowl/Vault/releases/latest/download/Vault.jar
 
 # EssentialsX (base jar)
-# curl -o EssentialsX.jar -L https://github.com/EssentialsX/Essentials/releases/latest/download/EssentialsX.jar
+curl -o EssentialsX.jar -L https://github.com/EssentialsX/Essentials/releases/latest/download/EssentialsX.jar
 
 # LuckPerms
-# curl -o LuckPerms.jar -L https://download.luckperms.net/1535/bukkit/loader/LuckPerms-Bukkit-5.4.128.jar
+curl -o LuckPerms.jar -L https://download.luckperms.net/1535/bukkit/loader/LuckPerms-Bukkit-5.4.128.jar
 
 # Citizens
-# curl -o Citizens.jar -L https://github.com/CitizensDev/Citizens2/releases/latest/download/Citizens.jar
+curl -o Citizens.jar -L https://github.com/CitizensDev/Citizens2/releases/latest/download/Citizens.jar
 
 # CommandNPC
-# curl -o CommandNPC.jar -L https://github.com/CitizensDev/CommandNPC/releases/latest/download/CommandNPC.jar
+curl -o CommandNPC.jar -L https://github.com/CitizensDev/CommandNPC/releases/latest/download/CommandNPC.jar
 
 # Quests
-# curl -o Quests.jar -L https://github.com/PikaMug/Quests/releases/latest/download/Quests.jar
+curl -o Quests.jar -L https://github.com/PikaMug/Quests/releases/latest/download/Quests.jar
 
 # PlayerPoints
-# curl -o PlayerPoints.jar -L https://github.com/PlayerPoints/PlayerPoints/releases/latest/download/PlayerPoints.jar
+curl -o PlayerPoints.jar -L https://github.com/PlayerPoints/PlayerPoints/releases/latest/download/PlayerPoints.jar
 
 # Parkour
-# curl -o Parkour.jar -L https://github.com/A5H73Y/Parkour/releases/latest/download/Parkour.jar
+curl -o Parkour.jar -L https://github.com/A5H73Y/Parkour/releases/latest/download/Parkour.jar
 
 # PlotSquared
-# curl -o PlotSquared.jar -L https://github.com/IntellectualSites/PlotSquared/releases/latest/download/PlotSquared-Bukkit.jar
+curl -o PlotSquared.jar -L https://github.com/IntellectualSites/PlotSquared/releases/latest/download/PlotSquared-Bukkit.jar
 
 # BentoBox
-# curl -o BentoBox.jar -L https://github.com/BentoBoxWorld/BentoBox/releases/latest/download/BentoBox.jar
+curl -o BentoBox.jar -L https://github.com/BentoBoxWorld/BentoBox/releases/latest/download/BentoBox.jar
 
 # BSkyBlock addon
-# curl -o BSkyBlock.jar -L https://github.com/BentoBoxWorld/addon-bskyblock/releases/latest/download/BSkyBlock.jar
+curl -o BSkyBlock.jar -L https://github.com/BentoBoxWorld/addon-bskyblock/releases/latest/download/BSkyBlock.jar
 
